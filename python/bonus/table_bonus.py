c8356b9f642fe36f6713e1012b880ed7a3df6781
Spokequenouille <spokequenouille@gmail.com>
2024-06-20 23:58:34 +0200

    fin table_bonus

--- python/bonus/table_bonus.py
+++ python/bonus/table_bonus.py
@@ -1,4 +1,6 @@
 ligne = int(input("Hauteur du triange : "))
 longueur = (ligne*2)-1
-for i in range(ligne):
-    print()
+for i in range(1,ligne+1):
+    nb_etoile=(i*2)-1
+    espace=(longueur-nb_etoile)
+    print(" "*(espace/2))+("*"*nb_etoile)+(" "*(espace/2))
